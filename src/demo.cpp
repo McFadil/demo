3a29cc06175c9105a31ae4aecceb827a52c9638e
McFadil <mfadil.job@gmail.com>
2020-10-25 16:53:45 +0100

    initial commit

---
+++ src/demo.cpp
@@ -0,0 +1,19 @@
+//============================================================================
+// Name        : demo.cpp
+// Author      : engineer
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+	cout << "this is a simple string" << endl;
+
+
+
+	return 0;
+}
